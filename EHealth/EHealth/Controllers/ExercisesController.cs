a2e1b7abef30b8dffcdd6e1f567adf8df7840e58
Mihai <rosian_mihai22@yahoo.com>
2019-08-18 01:43:10 +0300

    small change

--- EHealth/EHealth/Controllers/ExercisesController.cs
+++ EHealth/EHealth/Controllers/ExercisesController.cs
@@ -19,21 +19,52 @@ namespace EHealth.Controllers
             return View();
         }
 
-        public JsonResult Update(ExerciseViewModel model)
+        public JsonResult Save(ExerciseViewModel model)
+        {
+            var response = ResponseFactory.ErrorReponse;
+            if (model.Id == Guid.Empty)
+            {
+                response = Create(model);
+            }
+            else
+            {
+                response = Update(model);
+            }
+
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        public Response Update(ExerciseViewModel model)
         {
 
             if (!ModelState.IsValid)
             {
-                return Json(ResponseFactory.ErrorReponse);
+                return ResponseFactory.ErrorReponse;
             }
 
             var savedResponse = ExerciseCore.SaveExercises(model);
             if (savedResponse == null)
             {
-                return Json(ResponseFactory.ErrorReponse);
+                return ResponseFactory.ErrorReponse;
             }
 
-            return Json(ResponseFactory.SuccessResponse, JsonRequestBehavior.AllowGet);
+            return ResponseFactory.SuccessResponse;
+        }
+
+        public Response Create(ExerciseViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ResponseFactory.ErrorReponse;
+            }
+
+            var creationResponse = ExerciseCore.CreateExercises(model);
+            if (creationResponse == null)
+            {
+                return ResponseFactory.ErrorReponse;
+            }
+
+            return ResponseFactory.SuccessResponse;
         }
 
         public JsonResult Delete(Guid Id)
@@ -48,21 +79,7 @@ namespace EHealth.Controllers
             return Json(ResponseFactory.SuccessResponse, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult Create(ExerciseViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return Json(ResponseFactory.ErrorReponse);
-            }
 
-            var creationResponse = ExerciseCore.CreateExercises(model);
-            if (creationResponse == null)
-            {
-                return Json(ResponseFactory.ErrorReponse);
-            }
-
-            return Json(ResponseFactory.SuccessResponse, JsonRequestBehavior.AllowGet);
-        }
 
     }
 }
